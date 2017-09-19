fc878b768d028c6cf5cdc2cca19e3706be32d881
sravanthi velaga <svelaga0412@gmail.com>
2017-09-18 23:26:42 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+var values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+var suits = ["spades", "clubs", "hearts", "diamonds"];
+
+  suits.forEach(function(suit){
+    values.forEach(function(value){
+      // $(".output").append("<li>" + value + "  of " + suit + "</li>");
+    })
+  })
+
+
+
+});
