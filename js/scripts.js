48ec1856135eb5d1c3cfc3b81dd17fca3daa1763
Sravyy <31425335+Sravyy@users.noreply.github.com>
2017-09-20 02:19:09 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var suits = ["spades", "clubs", "hearts", "diamonds"];
 
   suits.forEach(function(suit){
     values.forEach(function(value){
-       $(".output").append("<li>" + value + "  of " + suit + "</li>");
+       $("#output ul").append("<li>" + value + "  of " + suit + "</li>");
     })
   })
 
